96ee0a045c9d36dfd85a762eee79011318b1bea7
Felipe Noronha <felipenoris@gmail.com>
2018-11-26 00:15:52 -0200

    benches

--- benches/bench.rs
+++ benches/bench.rs
@@ -18,5 +18,18 @@ fn bench_brsettlement(bench: &mut Bencher) {
     })
 }
 
-benchmark_group!(benches, bench_brsettlement);
+fn bench_brsettlement_cached(bench: &mut Bencher) {
+
+	let cal = bdays::brazil::BRSettlement;
+    let d0 = chrono::NaiveDate::from_ymd(2015, 06, 29);
+    let d1 = chrono::NaiveDate::from_ymd(2100, 12, 20);
+
+    let cached_cal = bdays::HolidayCalendarCache::new(cal, chrono::NaiveDate::from_ymd(1980, 1, 1), chrono::NaiveDate::from_ymd(2100, 12, 31));
+
+    bench.iter(|| {
+        cached_cal.bdays(d0, d1);
+    })
+}
+
+benchmark_group!(benches, bench_brsettlement, bench_brsettlement_cached);
 benchmark_main!(benches);
